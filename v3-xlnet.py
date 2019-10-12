c446147fc39ca43b13b67c83a252b7b48ff4d597
p.surya1994@gmail.com <penmetss@server.mila.quebec>
2019-10-11 23:29:23 -0400

    code for clnet.

---
+++ v3-xlnet.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from visdom import Visdom
+
+class VisdomLinePlotter(object):
+    """Plots to Visdom"""
+    def __init__(self, env_name='main'):
+        self.viz = Visdom()
+        self.env = env_name
+        self.plots = {}
+    def plot(self, var_name, split_name, title_name, x, y):
+        if var_name not in self.plots:
+            self.plots[var_name] = self.viz.line(X=np.array([x,x]), Y=np.array([y,y]), env=self.env, opts=dict(
+                legend=[split_name],
+                title=title_name,
+                xlabel='Epochs',
+                ylabel=var_name
+            ))
+        else:
+            self.viz.line(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.plots[var_name], name=split_name, update = 'append')
+            
+            
+    
+vis = VisdomLinePlotter()
+
+
+# In[2]:
+
+
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from pytorch_transformers import XLNetModel, XLNetTokenizer, XLNetForSequenceClassification
+from pytorch_transformers import AdamW
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+# In[3]:
+
+
+torch.cuda.is_available()
+
+
+# In[4]:
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+torch.cuda.get_device_name(0)
+# device="cpu"
+
+
+# In[5]:
+
+
+import numpy as np
+import math
+import pandas as pd
+
+import pandas as pd
+import numpy as np
+from scipy.stats import randint
+import seaborn as sns # used for plot interactive graph. 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import StringIO
+from sklearn.feature_extraction.text import *
+
+from sklearn.feature_selection import chi2
+from IPython.display import display
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.neural_network import MLPClassifier
+
+
+# In[6]:
+
+
+df = pd.read_csv('reddit_train.csv')
+df2 = pd.read_csv('reddit_test.csv')
+# df = df.sample(1000, random_state=1).copy()
+# df2 = df2.sample(1000, random_state=1).copy()
+df.head()
+
+
+# In[7]:
+
+
+df['category_id'], mapping = df['subreddits'].factorize()
+
+
+# In[8]:
+
+
+# Create sentence and label lists
+sentences = df.comments.values
+
+# We need to add special tokens at the beginning and end of each sentence for BERT to work properly
+sentences = [sentence + " [SEP] [CLS]" for sentence in sentences]
+labels = df.category_id.values
+
+
+# In[9]:
+
+
+tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
+
+
+# In[10]:
+
+
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+print ("Tokenize the first sentence:")
+print (tokenized_texts[0])
+
+
+# In[11]:
+
+
+# Set the maximum sequence length. The longest sequence in our training set is 47, but we'll leave room on the end anyway. 
+# In the original paper, the authors used a length of 512.
+MAX_LEN = 512
+
+
+# In[12]:
+
+
+# Use the BERT tokenizer to convert the tokens to their index numbers in the BERT vocabulary
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+
+
+# In[13]:
+
+
+# Pad our input tokens
+input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+
+# In[14]:
+
+
+# Create attention masks
+attention_masks = []
+
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+  seq_mask = [float(i>0) for i in seq]
+  attention_masks.append(seq_mask)
+
+
+# In[15]:
+
+
+# Use train_test_split to split our data into train and validation sets for training
+
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels, 
+                                                            random_state=2018, test_size=0.05)
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                             random_state=2018, test_size=0.05)
+
+
+# In[16]:
+
+
+# Convert all of our data into torch tensors, the required datatype for our model
+
+train_inputs = torch.tensor(train_inputs)
+validation_inputs = torch.tensor(validation_inputs)
+train_labels = torch.tensor(train_labels)
+validation_labels = torch.tensor(validation_labels)
+train_masks = torch.tensor(train_masks)
+validation_masks = torch.tensor(validation_masks)
+
+
+# In[17]:
+
+
+# Select a batch size for training. For fine-tuning BERT on a specific task, the authors recommend a batch size of 16 or 32
+batch_size = 8
+
+# Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop, 
+# with an iterator the entire dataset does not need to be loaded into memory
+
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+
+# In[18]:
+
+
+model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased", num_labels=20)
+model.cuda()
+
+
+# In[19]:
+
+
+
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
+
+
+# In[20]:
+
+
+# This variable contains all of the hyperparemeter information our training loop needs
+optimizer = AdamW(optimizer_grouped_parameters,
+                     lr=2e-5)
+
+
+# In[21]:
+
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+# In[ ]:
+
+
+# Store our loss and accuracy for plotting
+train_loss_set = []
+step = 0
+
+# Number of training epochs (authors recommend between 2 and 4)
+epochs = 4
+
+# trange is a tqdm wrapper around the normal python range
+for _ in trange(epochs, desc="Epoch"):
+    
+    
+    # Training
+    
+    # Set our model to training mode (as opposed to evaluation mode)
+    model.train()
+    
+    # Tracking variables
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    
+    # Train the data for one epoch
+    for batch in tqdm(train_dataloader,desc='batch',leave=False):
+        step = step + 1
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+        # Clear out the gradients (by default they accumulate)
+        optimizer.zero_grad()
+        # Forward pass
+        outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+        loss = outputs[0]
+        logits = outputs[1]
+        train_loss_set.append(loss.item()) 
+        vis.plot('loss', 'train_loss', 'Loss',step,loss.item())   
+        # Backward pass
+        loss.backward()
+        # Update parameters and take a step using the computed gradient
+        optimizer.step()
+        
+        
+        # Update tracking variables
+        tr_loss += loss.item()
+        nb_tr_examples += b_input_ids.size(0)
+        nb_tr_steps += 1            
+        # Validation
+
+        if(step%500==0):
+            print("Train loss: {}".format(tr_loss/nb_tr_steps))
+            # Put model in evaluation mode to evaluate loss on the validation set
+            model.eval()
+
+            # Tracking variables 
+            eval_loss, eval_accuracy = 0, 0
+            nb_eval_steps, nb_eval_examples = 0, 0
+
+            # Evaluate data for one epoch
+            for batch in validation_dataloader:
+                # Add batch to GPU
+                batch = tuple(t.to(device) for t in batch)
+                # Unpack the inputs from our dataloader
+                b_input_ids, b_input_mask, b_labels = batch
+                # Telling the model not to compute or store gradients, saving memory and speeding up validation
+                with torch.no_grad():
+                    # Forward pass, calculate logit predictions
+                    output = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+                    logits = output[0]
+                
+                # Move logits and labels to CPU
+                logits = logits.detach().cpu().numpy()
+                label_ids = b_labels.to('cpu').numpy()
+
+                tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+                
+                eval_accuracy += tmp_eval_accuracy
+                nb_eval_steps += 1
+
+            print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+            vis.plot('accuracy', 'val_acc', 'val_acc',step,eval_accuracy/nb_eval_steps)
+            torch.save(model, 'xlnet_model.pt')
+
